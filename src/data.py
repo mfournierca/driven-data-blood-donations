2de9a20673d5ae533f788605dcf728fe089ca2ec
Matthew Fournier <fournier.matthew@gmail.com>
2015-05-30 10:23:36 -0400

    normalize the dataframe
    Issue: master

--- src/data.py
+++ src/data.py
@@ -13,10 +13,15 @@ def download():
     urlretrieve(DATA_SOURCE_URL, DATA_FILE)
 
 
+def normalize(df):
+    return (df - df.mean()) / df.std()
+
+
 def load(random_seed=1, test_ratio=0.25):
     df = pd.read_csv(DATA_FILE, index_col=0)
     xtrain, xtest = train_test_split(
         df, random_state=random_seed, test_size=test_ratio)
     ytrain = xtrain.pop("Made Donation in March 2007")
     ytest = xtest.pop("Made Donation in March 2007")
-    return xtrain, ytrain, xtest, ytest
+    return normalize(xtrain), ytrain, normalize(xtest), ytest
+
