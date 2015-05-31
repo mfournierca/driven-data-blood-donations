ddbbc9a3d2f07c7775f4a1d580826934e6bc8b6c
Matthew Fournier <fournier.matthew@gmail.com>
2015-05-30 21:35:55 -0400

    add pca to pipeline
    Issue: master

--- src/model.py
+++ src/model.py
@@ -1,5 +1,5 @@
 import numpy
-from sklearn import pipeline
+from sklearn import pipeline, decomposition
 from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
@@ -9,18 +9,28 @@ def logloss(model, x, y):
     return log_loss(y, model.predict_proba(x))
 
 
-def model():
-    """Return a model. 
+def model(x, y):
+    """Return a model fitted to the training data.
 
     :return: a scikit-learn model
     :rtype: sklearn.pipeline
     """
-    steps = [("logistic", LogisticRegression())] 
+    steps = [
+        ("pca", decomposition.PCA()),
+        ("logistic", LogisticRegression(C=0.5))
+    ] 
     pipe = pipeline.Pipeline(steps=steps)
 
-    regularization_param_space = numpy.logspace(-4, 0)
+    regularization_param_space = numpy.logspace(-4, 1)
+    pca_n_components_space = range(1, len(x.columns))
+
     estimator = GridSearchCV(
         pipe,
-        {"logistic__C": regularization_param_space}
+        {
+            "logistic__C": regularization_param_space,
+            "pca__n_components": pca_n_components_space
+        }
     )
-    return estimator
+    estimator.fit(x, y)
+
+    return estimator.best_estimator_
